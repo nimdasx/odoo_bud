71d7f8d8e4a9058d996000a0a9e6068e0296c590
dev <dev@dev>
2023-12-19 17:48:20 +0700

    nampilin semua solvent per drug

--- models/sf_drug.py
+++ models/sf_drug.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SfDrug(models.Model):
@@ -13,11 +13,28 @@ class SfDrug(models.Model):
         required=True,
         tracking=True,
         help="Input drug name here")
-    description = fields.Text()
-
-    # list drug_bud, sf_drug_id dari tabel sf_drug_bud.sf_drug_id
-    sf_drug_bud_ids = fields.One2many("sf_drug_bud", "sf_drug_id", string="Beyond Use Date")
-
     _sql_constraints = [
         ('name_check', 'unique(name)', 'Drug Name must unique!')
     ]
+
+    description = fields.Text()
+
+    # list drug_bud, sf_drug_id dari tabel sf_drug_bud.sf_drug_id
+    # field ini gak ada di database
+    sf_drug_bud_ids = fields.One2many("sf_drug_bud", "sf_drug_id", string="Beyond Use Date")
+
+    # loop semua description yang ada di sf_drug_bud.description, ini untuk nampilin semua description
+    sf_drug_bud_descriptions = fields.Text(compute="_compute_sf_drug_bud_descriptions", string="BUD Descriptions")
+    @api.depends('sf_drug_bud_ids.description')
+    def _compute_sf_drug_bud_descriptions(self):
+        for record in self:
+            descriptions = record.sf_drug_bud_ids.mapped('description')
+            record.sf_drug_bud_descriptions = ', '.join(descriptions)
+
+    # loop semua solvent_name yang ada di sf_drug_bud.solvent_name, ini untuk nampilin semua solvent name
+    sf_drug_bud_solvent_names = fields.Char(compute="_compute_sf_drug_bud_solvent_name", string="Solvents")
+    @api.depends('sf_drug_bud_ids.solvent_name')
+    def _compute_sf_drug_bud_solvent_name(self):
+        for record in self:
+            solvent_names = record.sf_drug_bud_ids.mapped('solvent_name')
+            record.sf_drug_bud_solvent_names = ', '.join(solvent_names)
--- models/sf_drug_bud.py
+++ models/sf_drug_bud.py
@@ -7,5 +7,9 @@ class SfDrugBud(models.Model):
 
     sf_drug_id = fields.Many2one("sf_drug", required=True, ondelete='cascade')
     sf_solvent_id = fields.Many2one("sf_solvent", required=True, ondelete='cascade', string="Solvent")
+    description = fields.Text(string="Description")
     storage_temperature = fields.Integer(string="Storage Temperature (C)")
     expired_hours = fields.Integer()
+
+    # tambahkan nama solvent disini agar bisa dipanggil oleh sf_drug
+    solvent_name = fields.Char(related='sf_solvent_id.name', string="Solvent Name", store=True)
