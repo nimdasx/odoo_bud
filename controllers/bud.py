46a7aeee9aebaef6064880c3cd418d3fbd083d1a
dev <dev@dev>
2024-01-29 16:53:09 +0700

    sf_drug/get_list

---
+++ controllers/bud.py
@@ -0,0 +1,20 @@
+from odoo import http
+from odoo.http import request, Response
+import json
+
+class BudController(http.Controller):
+
+    @http.route('/sf_drug/get_list', type='http', auth='public')
+    def get_data(self):
+        # Logic to retrieve data from Odoo
+        # data = {
+        #     'message': 'Hello from Odoo JSON API',
+        #     'data': [1, 2, 3, 4, 5],
+        # }
+        xyz = request.env['sf_drug'].sudo().search([])
+        data = [{
+            'id':xy.id,
+            'name':xy.name,
+            'email':xy.description,
+        } for xy in xyz]
+        return Response(json.dumps(data), content_type='application/json')
